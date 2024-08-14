f34f500d9f427b4d01a3e8a1552bc8333fc3a368
Rafael Rodrigues <rafael.rodrigues@e-construmarket.com.br>
2024-08-14 17:49:41 -0300

    removi métodos desnecessários

--- src/FIAP.TECH.TESTS/Contacts/Setup/ContactTestsFixture.cs
+++ src/FIAP.TECH.TESTS/Contacts/Setup/ContactTestsFixture.cs
@@ -1,10 +1,6 @@
-﻿using AutoMapper;
-using Bogus;
-using FIAP.TECH.CORE.APPLICATION.Services.Contacts;
+﻿using Bogus;
 using FIAP.TECH.CORE.DOMAIN.Entities;
-using FIAP.TECH.CORE.DOMAIN.Interfaces.Repositories;
 using FIAP.TECH.CORE.DOMAIN.Validation;
-using Moq;
 
 namespace FIAP.TECH.TESTS.Contacts.Setup;
 
@@ -14,14 +10,12 @@ public class ContactFixtureCollection : ICollectionFixture<ContactTestsFixture> 
 public class ContactTestsFixture
 {
     public Contact _contact;
-    public ContactService contactService;
     private readonly Faker _faker;
 
     public ContactTestsFixture()
     {
         _faker = new Faker("pt_BR");
         _contact = GetContactSucess();
-        contactService = SetConfigService();
     }
 
     public Contact GetContactSucess()
@@ -74,15 +68,6 @@ public class ContactTestsFixture
 
     public ContactUpdateValidation GetValidationUpdate() => new();
 
-    public ContactService SetConfigService()
-    {
-        var mapper = new Mock<IMapper>();
-        var regionRepository = new Mock<IRegionRepository>();
-        var contactRepository = new Mock<IContactRepository>();
-
-        return new ContactService(mapper.Object, regionRepository.Object, contactRepository.Object);
-    }
-
     public List<Contact> GetListContact(string DDD)
     {
         var ddd = new List<string> { "11", "22", "33" };
@@ -94,20 +79,5 @@ public class ContactTestsFixture
             .RuleFor(p => p.PhoneNumber, _faker.Random.Replace("9#######"));
 
         return contacts.Generate(100).Where(x => x.DDD == DDD).ToList();
-    }
-}
-
-public static class TestsExtensions
-{
-    public static void AssignToken(this HttpClient httpClient, string token)
-    {
-        httpClient.AssignJsonMediaType();
-        httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
-    }
-
-    public static void AssignJsonMediaType(this HttpClient httpClient)
-    {
-        httpClient.DefaultRequestHeaders.Clear();
-        httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
     }
 }
